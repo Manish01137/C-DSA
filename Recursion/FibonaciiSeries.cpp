26928736297c6e919bef352c44ada650a0d62565
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-02-17 19:45:06 +0530

    Fibonacii

--- Recursion/FibonaciiSeries.cpp
+++ Recursion/FibonaciiSeries.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
+int Fibo(int n){
+   // base case
+   if(n==0)
+   return 0;
+  if (n==1)
+  return 1;
+   
+  int ans = Fibo(n-1)+Fibo(n-2); 
+  return ans;
+ 
+}
 int main(){
-    
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+     Fibo(n);
+   int ans = Fibo(n);
+   cout<<ans;
     return 0;
 }
