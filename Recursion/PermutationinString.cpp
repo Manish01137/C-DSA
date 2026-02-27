19bb6d4fa798e0fbf9fdb653152e22310e23ebe5
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-02-27 19:54:47 +0530

    done

---
+++ Recursion/PermutationinString.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+void solve (int nums,int anss,int index){
+    if(index>=nums.size()){
+        
+    }
+}
+int permutation(int arr[],int nums){
+    int anss[];
+    int index = 0;
+    solve(nums,ans,index)
+    return anss;
+}
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    int ans = permutation(arr,n);
+    cout<<ans;
+    return 0;
+}
