993304b1ba5b278f566f1e8bda95d438ff52da6a
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-03-20 10:28:52 +0530

    Opps concept

---
+++ OPPs/Encapsulation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Student{
+    private:
+    string name;
+    int age;
+    int height;
+    public:
+    int getAge(){
+        return this->age;
+    }
+};
+int main(){
+    Student first;
+    cout<<"Sab shi chal ra ha";
+    return 0;
+}
