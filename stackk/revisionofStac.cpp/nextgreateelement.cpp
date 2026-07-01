041c0c2934b0d0d8cc332e2580c325183ec484a7
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-07-01 11:30:37 +0530

    Next Greater Element

---
+++ stackk/revisionofStac.cpp/nextgreateelement.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+vector<int> nextGreaterElement(vector<int>& arr){
+    int n = arr.size();
+    
+    vector<int> ans(n);
+    stack<int> st;
+
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && st.top()<=arr[i]){
+            st.pop();
+        }
+        if(st.empty())
+          ans[i] = -1;
+        else
+          ans[i] = st.top();
+
+          st.push(arr[i]);
+    }
+    return ans;
+}
+int main(){
+    vector<int> arr = {2,1,4,3};
+    vector<int> ans = nextGreaterElement(arr);
+    for(int x: ans){
+        cout<<x<<" ";
+    }
+    return 0;
+}
