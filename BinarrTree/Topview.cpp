0172a63ccff52f522d09ff65b5eb117a700af5bd
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-07-09 17:27:09 +0530

    Top veiw

---
+++ BinarrTree/Topview.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <map>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+vector<int> topView(Node* root){
+    vector<int> ans;
+    if(root!==NULL){
+        return ans;
+    }
+    map<int,int> topNode;
+    queue<pair<Node*,int> > q;
+    q.push(make_pair(root,0));
+
+    while(!q.empty()){
+        pair<Node*,int> temp = q.front();
+        q.pop();
+        Node* frontNode = temp.first;
+        int hd = temp.second;
+
+        // if one value is prsent for a HD, then 
+        if(topNode.find(hd)==topNode){
+            topNode[hd] = frontNode->data;
+        }
+        if(frontNode->left){
+            q.push(make_pair(frontNode->left,hd-1));
+        }
+        if(frontNode->right){
+            q.push(make_pair(frontNode->right,hd+1));
+        }
+    }
+    for(auto i:topNode){
+        ans.push_back(i.second);
+    }
+    return ans;
+}
+Node* buildTree(){
+    int data;
+    cin>> data;
+    if(data == -1){
+        return NULL;
+    }
+    Node* root = new Node(data);
+    root->left = buildTree();
+    root->right = buildTree();
+
+    return root;
+}
+int main(){
+    
+    cout << "Enter Tree (Preorder with -1):" << endl;
+
+    Node* root = buildTree();
+
+    vector<int> ans = topView(root);
+
+    cout << "Top View: ";
+
+    for (int x : ans) {
+        cout << x << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
