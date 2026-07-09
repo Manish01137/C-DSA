097910f37e123c2ef034a933661664cc4117ee3b
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-07-09 12:49:31 +0530

    Boundary tree

--- BinarrTree/BoundaryTraversal.cpp
+++ BinarrTree/BoundaryTraversal.cpp
@@ -1,101 +1,127 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-class Node{
-    public:
+
+class Node {
+public:
     int data;
     Node* left;
     Node* right;
-    Node(int data){
+
+    Node(int data) {
         this->data = data;
         left = NULL;
         right = NULL;
     }
 };
-Node* buildTree(){
+
+// Build Tree
+Node* buildTree() {
+
     int data;
-    cin>>data;
-    if(data == -1){
+    cin >> data;
+
+    if (data == -1)
         return NULL;
-    }
+
     Node* root = new Node(data);
+
     root->left = buildTree();
     root->right = buildTree();
 
-    return ans;
+    return root;
 }
-void traverseleft(Node* root, vector<int> &ans){
-      // base case
-      if((root==NULL) || (root->left==NULL && root->right==NULL)){
-           return;
-      }
-      ans.push_back(root->data);
-      if(root->left)
-        traverseleft(root->left,ans);
-      else
-        traverseleft(root->right,ans);
-     
-}
-void traverseLeaf(Node* root, vector<int> &ans){
-    // base case
-    if(root== NULL){
+
+// Left Boundary
+void traverseLeft(Node* root, vector<int> &ans) {
+
+    if (root == NULL)
         return;
-    }
-    if(root->left == NULL && root->right == NULL){
+
+    // Ignore leaf node
+    if (root->left == NULL && root->right == NULL)
+        return;
+
+    ans.push_back(root->data);
+
+    if (root->left)
+        traverseLeft(root->left, ans);
+    else
+        traverseLeft(root->right, ans);
+}
+
+// Leaf Nodes
+void traverseLeaf(Node* root, vector<int> &ans) {
+
+    if (root == NULL)
+        return;
+
+    if (root->left == NULL && root->right == NULL) {
         ans.push_back(root->data);
         return;
     }
-    traverseLeaf(root->left,ans);
-    traverseLeaf(root->right,ans);
+
+    traverseLeaf(root->left, ans);
+    traverseLeaf(root->right, ans);
 }
-void traverseRight(Node* root,vector<int> &ans){
-    // base case
-    if((root==NULL) || (root->left==NULL || root->right==NULL)){
+
+// Right Boundary
+void traverseRight(Node* root, vector<int> &ans) {
+
+    if (root == NULL)
         return;
-    }
-    if(root->right){
-        traverseRight(root->right);
-    }
-    else{
-        traverseleft(root->left);
-    }
 
-    // wapas aagya
-    ans.push(root->data);
-  
+    // Ignore leaf node
+    if (root->left == NULL && root->right == NULL)
+        return;
+
+    if (root->right)
+        traverseRight(root->right, ans);
+    else
+        traverseRight(root->left, ans);
+
+    // Push while coming back
+    ans.push_back(root->data);
 }
-vector<int> boundary(Node* root){
-     vector<int> ans;
-     if(root === NULL){
+
+// Boundary Traversal
+vector<int> boundary(Node* root) {
+
+    vector<int> ans;
+
+    if (root == NULL)
         return ans;
-     }
-     ans.push_back(root->left);
 
-     // left part print/store
-     traverseleft(root->left,ans);
+    // Root
+    ans.push_back(root->data);
 
-     // travesleaf node
+    // Left Boundary
+    traverseLeft(root->left, ans);
 
-     // left subtree
-     traverseLeaf(root->left,ans);
-     // right subtree
-     traverseLeaf(root->right,ans);
+    // Leaf Nodes
+    traverseLeaf(root->left, ans);
+    traverseLeaf(root->right, ans);
 
-     // traverse rigth
-     traveseRight(root->right,ans);
-     return ans;
+    // Right Boundary
+    traverseRight(root->right, ans);
 
+    return ans;
 }
-int main(){
+
+int main() {
+
+    cout << "Enter Tree (Preorder with -1):" << endl;
+
     Node* root = buildTree();
 
     vector<int> ans = boundary(root);
 
-    cout << "Boundary: ";
+    cout << "Boundary Traversal: ";
 
-    for (int i : ans) {
-        cout << i << " ";
-    }
+    for (int x : ans)
+        cout << x << " ";
 
     cout << endl;
+
     return 0;
 }
